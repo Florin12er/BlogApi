585a0cc11a42899ec28ee8dae882e65df07dca1c
florin <apetreiflorin236@gmail.com>
2024-07-02 17:38:18 +0200

    added the authefication pages ui, added the create blog page ui

--- routes/users/users.js
+++ routes/users/users.js
@@ -1,16 +1,21 @@
+// router.js
 const express = require("express");
 const router = express.Router();
-const passport = require("passport");
-const { checkNotAuthenticated, checkAuthenticated } = require("../../middleware/checks.js");
-const PostLogin = require("./login.js"); 
+const PostLogin = require("./login.js"); // Assuming this file exists and exports something
 const { PostRegister } = require("./register.js");
 const User = require("../../models/User.js");
 const LogOut = require("./logout.js");
 const { Reset, requestPasswordReset } = require("./reset.js");
 const ShowAllUsers = require("./showAll.js");
+const passport = require("passport");
 const DeleteUser = require("./deleteUser.js");
 
-router.get("/", checkAuthenticated, ShowAllUsers);
+const {
+  checkNotAuthenticated,
+  checkAuthenticated,
+} = require("../../middleware/checks.js");
+
+router.get("/", ShowAllUsers);
 router.post("/register", checkNotAuthenticated, PostRegister);
 router.post("/login", checkNotAuthenticated, PostLogin);
 router.delete("/logout", LogOut);
@@ -18,40 +23,33 @@ router.delete("/delete", checkAuthenticated, DeleteUser);
 router.post("/reset", Reset);
 router.post("/request-reset", requestPasswordReset);
 
-// Route to check if the user is authenticated
-router.get("/auth/status", (req, res) => {
-  if (req.isAuthenticated()) {
-    res.status(200).json({ authenticated: true, user: req.user });
-  } else {
-    res.status(401).json({ authenticated: false });
-  }
-});
-
 router.get(
   "/auth/google",
   passport.authenticate("google", { scope: ["profile", "email"] }),
 );
 
+// Google callback route
 router.get(
   "/auth/google/callback",
   passport.authenticate("google", { failureRedirect: "/login" }),
   (req, res) => {
-    res.redirect("/dashboard"); 
+    res.redirect("/dashboard"); // Redirect to dashboard or desired route
   },
 );
 
+// Login with GitHub
 router.get(
   "/auth/github",
   passport.authenticate("github", { scope: ["user:email"] }),
 );
 
+// GitHub callback route
 router.get(
   "/auth/github/callback",
   passport.authenticate("github", { failureRedirect: "/login" }),
   (req, res) => {
-    res.redirect("/dashboard"); 
+    res.redirect("/dashboard"); // Redirect to dashboard or desired route
   },
 );
 
 module.exports = router;
-
