b548dc18e2c7fe55bfd9cc850e5b1890ba54ec85
florin <apetreiflorin236@gmail.com>
2024-07-02 14:07:29 +0200

    added cors

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const initializePassport = require("./config/passport-config.js");
 const corsOptions = {
   origin: "http://localhost:5173",
   methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
   allowedHeaders: ["Content-Type", "Authorization"],
 };
 app.use(cors(corsOptions));
