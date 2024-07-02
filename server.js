b27786aecfb5a8980062d000fe5d8763b4862d2b
florin <apetreiflorin236@gmail.com>
2024-07-02 23:57:55 +0200

    added jwt to the api

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const jwt = require("jsonwebtoken");
 const initializePassport = require("./config/passport-config.js");
 
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: "*",
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
   allowedHeaders: ["Content-Type", "Authorization"],
