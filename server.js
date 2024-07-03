57826168e3663204bf1a4daf3f1c44f93b4ace4d
florin <apetreiflorin236@gmail.com>
2024-07-03 11:14:32 +0200

    added github and google authentication

--- server.js
+++ server.js
@@ -12,6 +12,8 @@ const corsOptions = {
   origin: [
     "https://blog-maker-two.vercel.app",
     "https://blogs-nine-steel.vercel.app",
+    "https://accounts.google.com/o/oauth2/v2/auth/oauthchooseaccount?response_type=code&redirect_uri=https%3A%2F%2Fblogapi-production-fb2f.up.railway.app%2Fuser%2Fauth%2Fgoogle%2Fcallback&scope=profile%20email&client_id=962618277009-68tn6hdcv2mc6upm8o775rt11e42ose9.apps.googleusercontent.com&service=lso&o2v=2&ddm=0&flowName=GeneralOAuthFlow",
+    "https://github.com/login/oauth/authorize?response_type=code&redirect_uri=https%3A%2F%2Fblogapi-production-fb2f.up.railway.app%2Fuser%2Fauth%2Fgithub%2Fcallback&scope=user%3Aemail&client_id=Ov23liYo0Lw0r3Y869gT",
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
