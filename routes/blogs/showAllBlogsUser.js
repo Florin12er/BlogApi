1077998dac9f6fa05af846aa880cdc4dbdbb1f4a
florin <apetreiflorin236@gmail.com>
2024-07-03 17:39:19 +0200

    added the ability to get a blog by id

--- routes/blogs/showAllBlogsUser.js
+++ routes/blogs/showAllBlogsUser.js
@@ -2,7 +2,7 @@ const Blog = require("../../models/Blog");
 
 async function ShowAllUserBlogs(req, res) {
   try {
-    const username = req.user.username; // Assuming your User model has _id field
+    const username = req.user._id; // Assuming your User model has _id field
 
     // Find all blogs where author matches the authenticated user's ID
     const blogs = await Blog.find({ author: username });
