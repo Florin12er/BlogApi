dc9e24e783d543baf16a5ff17da68b9ca34f9eb4
florin <apetreiflorin236@gmail.com>
2024-07-04 14:02:07 +0200

    added the ability to change the setting of the user

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const corsOptions = {
     "https://blogs-nine-steel.vercel.app",
     "https://blogapi-production-fb2f.up.railway.app",
   ],
-  methods: ["GET", "POST", "PUT", "DELETE"],
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   credentials: true,
   allowedHeaders: ["Content-Type", "Authorization"],
 };
