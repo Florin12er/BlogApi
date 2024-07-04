225c839aac94a23a255128b8ceecc9e9215b14c3
florin <apetreiflorin236@gmail.com>
2024-07-04 15:22:41 +0200

    added the ability to change the setting of the user

--- models/Blog.js
+++ models/Blog.js
@@ -2,17 +2,16 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const blogSchema = new Schema({
-  author: { type: String }, // Added required: true to author field
+  author: { type: String, required: true }, // Added required: true to author field
   title: { type: String, required: true },
   links: { type: String, required: true },
   tags: { type: String, required: true },
   content: { type: String, required: true },
   createdAt: { type: Date, default: Date.now },
-  likes: [{ type: Schema.Types.ObjectId, ref: 'User' }], // Array of user IDs who liked the blog
-  dislikes: [{ type: Schema.Types.ObjectId, ref: 'User' }] // Array of user IDs who disliked the blog
+  likes: [{ type: Schema.Types.ObjectId, ref: "User" }], // Array of user IDs who liked the blog
+  dislikes: [{ type: Schema.Types.ObjectId, ref: "User" }], // Array of user IDs who disliked the blog
 });
 
 const Blog = mongoose.model("Blog", blogSchema);
 
 module.exports = Blog;
-
--- routes/blogs/showAllBlogsUser.js
+++ routes/blogs/showAllBlogsUser.js
@@ -1,14 +1,9 @@
 const Blog = require("../../models/Blog");
-const jwt = require("jsonwebtoken");
 
 async function ShowAllUserBlogs(req, res) {
   try {
-    // Extract token from authorization header
-    const token = req.headers.authorization.split(" ")[1];
-    
-    // Verify the token to get user ID
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    const userId = decoded.id;
+    // Assuming req.user contains the authenticated user object with _id
+    const userId = req.user._id;
 
     // Fetch blogs authored by the user
     const blogs = await Blog.find({ author: userId });
