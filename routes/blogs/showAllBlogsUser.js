e63b8276216a14029dda78ec4dcfd21f559793b2
florin <apetreiflorin236@gmail.com>
2024-07-04 15:14:06 +0200

    added the ability to change the setting of the user

--- routes/blogs/showAllBlogsUser.js
+++ routes/blogs/showAllBlogsUser.js
@@ -3,11 +3,17 @@ const jwt = require("jsonwebtoken");
 
 async function ShowAllUserBlogs(req, res) {
   try {
+    // Extract token from authorization header
     const token = req.headers.authorization.split(" ")[1];
+    
+    // Verify the token to get user ID
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     const userId = decoded.id;
 
+    // Fetch blogs authored by the user
     const blogs = await Blog.find({ author: userId });
+
+    // Respond with the blogs
     res.status(200).json(blogs);
   } catch (error) {
     console.error("Error fetching user blogs:", error);
