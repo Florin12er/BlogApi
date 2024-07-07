d5e2ad946b0abb29be1dd3906ca60932b35fc0b3
florin <apetreiflorin236@gmail.com>
2024-07-07 20:05:36 +0200

    add checks to check if the user owns the comment

--- routes/blogs/comments.js
+++ routes/blogs/comments.js
@@ -11,10 +11,6 @@ async function PostComment(req, res) {
       return res.status(404).json({ message: "Blog not found" });
     }
 
-    const user = await User.findById(req.user._id); // Fetching the authenticated user
-    if (!user) {
-      return res.status(404).json({ message: "User not found" });
-    }
     const newComment = {
       user: userId,
       content,
@@ -25,11 +21,7 @@ async function PostComment(req, res) {
 
     res
       .status(201)
-      .json({
-        message: "Comment added successfully",
-        comment: newComment,
-        username: user.username,
-      });
+      .json({ message: "Comment added successfully", comment: newComment });
   } catch (error) {
     res.status(500).json({ message: "Error adding comment", error });
   }
@@ -57,40 +49,29 @@ async function UpdateComment(req, res) {
     res.status(500).json({ message: "Error updating comment", error });
   }
 }
-async function DeleteComment(req, res) {
-  const { blogId, commentId } = req.params;
+async function DeletComment(req, res) {
+  const userId = req.user._id; // Assuming req.user has the authenticated user object
 
   try {
-    const blog = await Blog.findById(blogId);
+    const blog = await Blog.findById(req.params.blogId);
     if (!blog) {
       return res.status(404).json({ message: "Blog not found" });
     }
 
-    const comment = blog.comments.id(commentId);
+    const comment = blog.comments.id(req.params.commentId);
     if (!comment) {
       return res.status(404).json({ message: "Comment not found" });
     }
 
     // Check if the authenticated user is the owner of the comment
-    if (comment.user.toString() !== req.user._id.toString()) {
-      return res
-        .status(403)
-        .json({ message: "Unauthorized to delete this comment" });
-    }
-
     comment.deleteOne();
     await blog.save();
 
     res.status(200).json({ message: "Comment deleted successfully" });
   } catch (error) {
-    console.error("Error deleting comment:", error);
     res.status(500).json({ message: "Error deleting comment", error });
   }
 }
-
-module.exports = {
-  DeleteComment,
-};
 
 async function ShowAllComments(req, res) {
   try {
@@ -107,4 +88,4 @@ async function ShowAllComments(req, res) {
     res.status(500).json({ message: "Error retrieving comments", error });
   }
 }
-module.exports = { PostComment, UpdateComment, DeleteComment, ShowAllComments };
+module.exports = { PostComment, UpdateComment, DeletComment, ShowAllComments };
