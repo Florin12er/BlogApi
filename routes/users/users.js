e30e3176daacdc06efd7e217ac3986fffcbc3d76
florin <apetreiflorin236@gmail.com>
2024-07-09 11:53:15 +0200

    added the ability to generate api keys and added the routes

--- routes/users/users.js
+++ routes/users/users.js
@@ -159,13 +159,20 @@ router.patch(
 // Generate API key route (protected)
 router.post("/generate-api-key", checkAuthenticated, async (req, res) => {
   try {
+    const userId = req.user._id;
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
     const apiKey = generateApiKey();
-    req.user.apiKey = apiKey;
-    await req.user.save();
+    user.apiKey = apiKey;
+    await user.save();
+
     res.status(200).json({ apiKey });
   } catch (error) {
     console.error("Error generating API key:", error);
     res.status(500).json({ error: "Failed to generate API key" });
   }
-});
+})
 module.exports = router;
