95e1a6ebd9212ba1bfc38a8a31f41238c27c4d88
florin <apetreiflorin236@gmail.com>
2024-07-10 14:37:28 +0200

    added cors for the api key

--- middleware/checks.js
+++ middleware/checks.js
@@ -4,31 +4,31 @@ const Blog = require("../models/Blog");
 const jwt = require("jsonwebtoken");
 const User = require("../models/User");
 
-
 const checkAuthenticated = async (req, res, next) => {
   const authHeader = req.headers.authorization;
 
-  if (!authHeader || !authHeader.startsWith("Bearer ")) {
-    return res.status(401).json({ error: "Unauthorized" });
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    return res.status(401).json({ error: 'Unauthorized' });
   }
 
-  const token = authHeader.split(" ")[1];
+  const token = authHeader.split(' ')[1];
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     const user = await User.findById(decoded.id);
 
     if (!user) {
-      return res.status(404).json({ error: "User not found" });
+      return res.status(404).json({ error: 'User not found' });
     }
 
     req.user = user; // Set req.user with the user object
     next();
   } catch (error) {
-    console.error("Error verifying JWT token:", error);
-    return res.status(401).json({ error: "Unauthorized" });
+    console.error('Error verifying JWT token:', error);
+    return res.status(401).json({ error: 'Unauthorized' });
   }
 };
+
 
 function checkNotAuthenticated(req, res, next) {
   const authHeader = req.headers.authorization;
@@ -72,29 +72,32 @@ async function checkCommentOwnership(req, res, next) {
 }
 
 const authenticateApiKey = async (req, res, next) => {
-  const apiKey = req.headers["x-api-key"];
+  const apiKey = req.headers['x-api-key'];
 
   if (!apiKey) {
-    return res.status(401).json({ message: "API key missing" });
+    return res.status(401).json({ message: 'API key missing' });
   }
 
   try {
-    const user = req.user; // Use req.user directly
+    const user = req.user;
+
+    if (!user || typeof user.decryptText !== 'function') {
+      return res.status(401).json({ message: 'Invalid user or decryptText method not available' });
+    }
 
     const decryptedApiKey = user.decryptText(user.apiKey);
     if (apiKey !== decryptedApiKey) {
-      return res.status(401).json({ message: "Invalid API key" });
+      return res.status(401).json({ message: 'Invalid API key' });
     }
 
-    // Optionally, you can store decrypted API key in req.user for later use
-    req.decryptedApiKey = decryptedApiKey;
-
+    req.decryptedApiKey = decryptedApiKey; // Optionally store decrypted API key
     next();
   } catch (error) {
-    console.error("Error authenticating API key:", error);
-    return res.status(500).json({ message: "Internal server error" });
+    console.error('Error authenticating API key:', error);
+    return res.status(500).json({ message: 'Internal server error' });
   }
 };
+
 // Middleware to check if the user is a guest
 const checkGuest = (req, res, next) => {
   if (req.user.isGuest) {
