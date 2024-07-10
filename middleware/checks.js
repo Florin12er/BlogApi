c6e45f7f28d5033bbfe84145e69b90352a5da9cc
florin <apetreiflorin236@gmail.com>
2024-07-10 14:26:48 +0200

    added cors for the api key

--- middleware/checks.js
+++ middleware/checks.js
@@ -73,29 +73,29 @@ async function checkCommentOwnership(req, res, next) {
 
 const authenticateApiKey = async (req, res, next) => {
   const apiKey = req.headers["x-api-key"];
-  
+
   if (!apiKey) {
     return res.status(401).json({ message: "API key missing" });
   }
 
   try {
-    const user = await User.findById(req.user.id);
-    if (!user) {
-      return res.status(404).json({ error: "User not found" });
-    }
+    const user = req.user; // Use req.user directly
 
     const decryptedApiKey = user.decryptText(user.apiKey);
     if (apiKey !== decryptedApiKey) {
       return res.status(401).json({ message: "Invalid API key" });
     }
 
-    req.user = user;
+    // Optionally, you can store decrypted API key in req.user for later use
+    req.decryptedApiKey = decryptedApiKey;
+
     next();
   } catch (error) {
     console.error("Error authenticating API key:", error);
     return res.status(500).json({ message: "Internal server error" });
   }
 };
+
 // Middleware to check if the user is a guest
 const checkGuest = (req, res, next) => {
   if (req.user.isGuest) {
