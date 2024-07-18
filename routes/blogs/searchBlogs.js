f85de410b10ca5b7eb32a1efe5dc258292835e2c
Florin12er <apetreiflorin236@gmail.com>
2024-07-18 20:43:04 +0200

    implement blog search

--- routes/blogs/searchBlogs.js
+++ routes/blogs/searchBlogs.js
@@ -1,29 +1,25 @@
-const Blog = require('../../models/Blog');
+const Blog = require("../../models/Blog");
 
 async function searchBlogs(req, res) {
   try {
-    const { query } = req.query;  // Get the search query from the request
-
+    const { query } = req.query;
     if (!query) {
       return res.status(400).json({ message: "Search query is required" });
     }
+
     const blogs = await Blog.find(
       { $text: { $search: query } },
-      { score: { $meta: "textScore" } }
+      { score: { $meta: "textScore" } },
     )
-    .sort({ score: { $meta: "textScore" } })
-    .limit(10);  // Limit to 10 results, adjust as needed
-
-    if (blogs.length === 0) {
-      return res.status(404).json({ message: "No blogs found matching the search query" });
-    }
+      .sort({ score: { $meta: "textScore" } })
+      .limit(10);
 
     res.json(blogs);
   } catch (error) {
-    console.error('Error searching blogs:', error);
-    res.status(500).json({ message: "An error occurred while searching blogs" });
+    console.error("Error searching blogs:", error);
+    res
+      .status(500)
+      .json({ message: "An error occurred while searching blogs" });
   }
 }
-
 module.exports = searchBlogs;
-
